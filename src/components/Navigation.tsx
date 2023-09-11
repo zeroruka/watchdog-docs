6c6b767f5268f689d6e691111e53f93552b15ba9
zeroruka <89210318+zeroruka@users.noreply.github.com>
2023-09-11 15:31:40 +0800

    update links

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -263,7 +263,11 @@ export function Navigation(props: React.ComponentPropsWithoutRef<'nav'>) {
           />
         ))}
         <li className="sticky bottom-0 z-10 mt-6 min-[416px]:hidden">
-          <Button href="#" variant="filled" className="w-full">
+          <Button
+            href="https://watchdog.zeroruka.moe/login"
+            variant="filled"
+            className="w-full"
+          >
             Sign in
           </Button>
         </li>
